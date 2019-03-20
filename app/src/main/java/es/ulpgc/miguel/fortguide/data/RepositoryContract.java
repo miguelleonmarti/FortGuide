ee608807352b7831ae38b9af08602bd03f52e170
Kilian <kilian.garcia106@alu.ulpgc.es>
2019-03-20 14:30:55 +0000

    Cambios en el ChallengeRepository

--- app/src/main/java/es/ulpgc/miguel/fortguide/data/RepositoryContract.java
+++ app/src/main/java/es/ulpgc/miguel/fortguide/data/RepositoryContract.java
@@ -1,4 +1,8 @@
 package es.ulpgc.miguel.fortguide.data;
 
+import java.util.List;
+
 public interface RepositoryContract {
+List<ChallengeItem> getChallengeList();
+
 }
--- app/src/mock/java/es.ulpgc.miguel.fortguide.challenge/ChallengeRepository.java
+++ app/src/mock/java/es.ulpgc.miguel.fortguide.challenge/ChallengeRepository.java
@@ -1,5 +1,27 @@
 package es.ulpgc.miguel.fortguide.challenge;
 
-public class ChallengeRepository {
+import java.util.ArrayList;
+import java.util.List;
 
+import es.ulpgc.miguel.fortguide.data.ChallengeItem;
+import es.ulpgc.miguel.fortguide.data.RepositoryContract;
+
+public class ChallengeRepository implements RepositoryContract {
+
+private static ChallengeRepository INSTANCE;
+
+    private final List<ChallengeItem> itemList = new ArrayList<>();
+    private final int COUNT = 20;
+
+public static  RepositoryContract getInstance(){
+    if(INSTANCE == null){
+        INSTANCE = new ChallengeRepository();
+    }
+    return INSTANCE;
+}
+
+    @Override
+    public List<ChallengeItem> getChallengeList() {
+        return null;
+    }
 }
