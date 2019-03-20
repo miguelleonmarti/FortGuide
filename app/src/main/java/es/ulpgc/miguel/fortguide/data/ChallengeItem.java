1ec299f114fe84780d6dae0e56ec546354e01c50
Kilian <kilian.garcia106@alu.ulpgc.es>
2019-03-20 12:55:19 +0000

    Implementacion de ChallengeItem

--- app/src/main/java/es/ulpgc/miguel/fortguide/data/ChallengeItem.java
+++ app/src/main/java/es/ulpgc/miguel/fortguide/data/ChallengeItem.java
@@ -1,4 +1,24 @@
 package es.ulpgc.miguel.fortguide.data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ChallengeItem {
+
+    public final List<ChallengeItem> items;
+    public final int id;
+    public final String content;
+
+
+    public ChallengeItem(int id, String content) {
+        this.id = id;
+        this.content = content;
+        items = new ArrayList<>();
+    }
+
+    @Override
+    public String toString() {
+        return content;
+    }
 }
+
