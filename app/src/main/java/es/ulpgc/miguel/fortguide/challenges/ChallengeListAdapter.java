69c1280c03ed021094ff6e4e684ae8fa76ada455
Kilian <kilian.garcia106@alu.ulpgc.es>
2019-03-20 13:09:36 +0000

    Implementacion de los metodos addItem y addItems en el ChallengeListAdapter

--- app/src/main/java/es/ulpgc/miguel/fortguide/challenges/ChallengeListAdapter.java
+++ app/src/main/java/es/ulpgc/miguel/fortguide/challenges/ChallengeListAdapter.java
@@ -1,4 +1,31 @@
 package es.ulpgc.miguel.fortguide.challenges;
 
-public class ChallengeListAdapter {
+import android.content.Context;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import es.ulpgc.miguel.fortguide.data.ChallengeItem;
+
+public class ChallengeListAdapter extends ArrayAdapter<ChallengeItem> {
+
+    private List<ChallengeItem> itemList = new ArrayList();
+    private final View.OnClickListener clickListener;
+
+public ChallengeListAdapter (Context context, View.OnClickListener listener){
+    super(context,0,new ArrayList());
+    clickListener = listener;
+}
+
+public void addItem(ChallengeItem item){
+    itemList.add(item);
+    notifyDataSetChanged();
+}
+
+public void addItems(List<ChallengeItem> items){
+    itemList = items;
+    notifyDataSetChanged();
+}
 }
