973f1d438d6ce6999f0ba6ddab570e84d8799d77
Kilian <kilian.garcia106@alu.ulpgc.es>
2019-03-29 13:04:05 +0000

    Implementada clase ChallengeItem

--- app/src/main/java/es/ulpgc/miguel/fortguide/data/ChallengeItem.java
+++ app/src/main/java/es/ulpgc/miguel/fortguide/data/ChallengeItem.java
@@ -1,4 +1,16 @@
 package es.ulpgc.miguel.fortguide.data;
 
-public class ChallengeItem {
+public class ChallengeItem extends ChallengeAbstract {
+
+  public final String details;
+
+  public ChallengeItem(int id, String content, String details) {
+    super(id, content);
+    this.details = details;
+  }
+
+    @Override
+    public String toString() {
+      return super.toString();
+    }
 }
