5a92acc65cbc3e339a15c32b6a88d71aec0051c3
Kilian <kilian.garcia106@alu.ulpgc.es>
2019-04-09 17:51:05 +0100

    Test en duda en NewTheoryPresenter

--- app/src/test/java/es/ulpgc/miguel/fortguide/NewTheoryPresenterMockitoTest.java
+++ app/src/test/java/es/ulpgc/miguel/fortguide/NewTheoryPresenterMockitoTest.java
@@ -16,43 +16,53 @@ import es.ulpgc.miguel.fortguide.data.AdviceItem;
 import es.ulpgc.miguel.fortguide.newTheory.NewTheoryContract;
 import es.ulpgc.miguel.fortguide.newTheory.NewTheoryPresenter;
 import es.ulpgc.miguel.fortguide.newTheory.NewTheoryState;
+import es.ulpgc.miguel.fortguide.newTheory.NewTheoryViewModel;
 
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-  @RunWith(MockitoJUnitRunner.class)
-  public class NewTheoryPresenterMockitoTest {
+@RunWith(MockitoJUnitRunner.class)
+public class NewTheoryPresenterMockitoTest {
 
-    @Mock
-    private NewTheoryContract.Model modelMock;
+  @Mock
+  private NewTheoryContract.Model modelMock;
 
-    @Mock
-    private NewTheoryContract.View viewMock;
+  @Mock
+  private NewTheoryContract.View viewMock;
 
-    @Mock
-    private NewTheoryContract.Router routerMock;
+  @Mock
+  private NewTheoryContract.Router routerMock;
 
-    private NewTheoryContract.Presenter presenter;
+  private NewTheoryContract.Presenter presenter;
 
-    @Before
-    public void setupNewTheoryScreen() {
+  @Before
+  public void setupNewTheoryScreen() {
 
-      // To inject the mocks in the test this method needs to be called
-      MockitoAnnotations.initMocks(this);
+    // To inject the mocks in the test this method needs to be called
+    MockitoAnnotations.initMocks(this);
 
-      // Get a reference to the class under test
-      presenter = new NewTheoryPresenter(new NewTheoryState());
+    // Get a reference to the class under test
+    presenter = new NewTheoryPresenter(new NewTheoryState());
 
-      // Inject dependencies to the class under test
-      presenter.injectView(new WeakReference<>(viewMock));
-      presenter.injectModel(modelMock);
-      presenter.injectRouter(routerMock);
+    // Inject dependencies to the class under test
+    presenter.injectView(new WeakReference<>(viewMock));
+    presenter.injectModel(modelMock);
+    presenter.injectRouter(routerMock);
 
-    }
+  }
 
-    @Test
-    public void goToMenuScreen() {
-      presenter.startMenuScreen();
-      verify(routerMock).navigateToMenuScreen();
-    }
+  @Test
+  public void goToMenuScreen() {
+    presenter.startMenuScreen();
+    verify(routerMock).navigateToMenuScreen();
+  }
+
+  /*
+  @Test
+  public void fetchNewTheoryData() {
+    NewTheoryViewModel newTheoryViewModel = new NewTheoryViewModel();
+    presenter.fetchData();
+    verify(viewMock).displayData(newTheoryViewModel);
+  }
+  */
 }
