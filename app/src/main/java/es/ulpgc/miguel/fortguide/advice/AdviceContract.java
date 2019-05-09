8c2d5b2cbe1b8b38f1130f533aecfd0d06a9b8a0
miguelleonmarti <miguelleonmarti@gmail.com>
2019-05-09 08:35:02 +0100

    AdviceContract commented

--- app/src/main/java/es/ulpgc/miguel/fortguide/advice/AdviceContract.java
+++ app/src/main/java/es/ulpgc/miguel/fortguide/advice/AdviceContract.java
@@ -8,36 +8,88 @@ import es.ulpgc.miguel.fortguide.data.RepositoryContract;
 public interface AdviceContract {
 
   interface View {
+    /**
+     * Dependency injection
+     * @param presenter Presenter
+     */
     void injectPresenter(Presenter presenter);
 
+    /**
+     * Display the data that has been fetch on the UI
+     * @param viewModel ViewModel
+     */
     void displayData(AdviceViewModel viewModel);
   }
 
   interface Presenter {
+    /**
+     * Dependency injection
+     * @param view View
+     */
     void injectView(WeakReference<View> view);
 
+    /**
+     * Dependency injection
+     * @param model Model
+     */
     void injectModel(Model model);
 
+    /**
+     * Dependency injection
+     * @param router Router
+     */
     void injectRouter(Router router);
 
+    /**
+     * Fetch data (the list of items) when it is required
+     * It is an async task
+     */
     void fetchData();
 
+    /**
+     * When a piece of advice is pressed
+     * @param item AdviceItem
+     */
     void selectAdviceListData(AdviceItem item);
 
+    /**
+     * Going to the menu screen when the bananaButton is pressed
+     */
     void startMenuScreen();
   }
 
   interface Model {
+    /**
+     * Calls the repository in order to the fetch data (advice list
+     * It is an async task
+     * @param callback GetAdviceListCallback
+     */
     void fetchAdviceListData(RepositoryContract.GetAdviceListCallback callback);
   }
 
   interface Router {
+    /**
+     * Navigate to the next screen (AdviceDetail)
+     */
     void navigateToAdviceDetailScreen();
 
+    /**
+     * Used to pass the advice item selected on the list to the
+     * next screen
+     * @param item AdviceItem
+     */
     void passDataToAdviceDetailScreen(AdviceItem item);
 
+    /**
+     * Recovering the advice state (which it has the list)
+     * in case the fetch data has been done before
+     * @return AdviceState
+     */
     AdviceState getDataFromPreviousScreen();
 
+    /**
+     * Intent to the menu screen
+     */
     void navigateToMenuScreen();
   }
 }
